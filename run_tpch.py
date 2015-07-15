eb4c4d89d027a13daade1d8e23f44c61aa1f7c8d
Brandon Myers <bdmyers@cs.washington.edu>
2015-07-14 17:43:41 -0700

    tpch running script for grappa

--- run_tpch.py
+++ run_tpch.py
@@ -1,1 +1,29 @@
-__author__ = 'brandon'
+import json
+import sys
+import subprocess
+
+grappa_srun = '../../bin/grappa_srun'
+
+cmd_template = """{0} \
+                        --ppn={{ppn}} \
+                        --nnode={{nnode}} \
+                        -- \
+                        ./{{exe}} \
+                        2>&1""".format(grappa_srun)
+
+for trial in [1, 2, 3, 4]:
+    for qn in [1, 2, 3]:
+        params = {
+            'exe': "grappa_tpc_q{0}.exe".format(qn),
+            'sf': 10,
+            'ppn': 12,
+            'nnode': 8,
+            'query': 'q{0}'.format(qn),
+            'trial': trial,
+        }
+
+        paramsjson = json.dumps(params)
+        print "PARAMS{0}PARAMS".format(paramsjson)
+
+        cmd = cmd_template.format(params)
+        subprocess.check_call(cmd, stderr=sys.stdout)
