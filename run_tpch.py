8c24ae93045936550b2ecf056c1eca72da4b8697
Brandon Myers <bdmyers@cs.washington.edu>
2015-07-31 12:59:45 -0700

    pal experiment too

--- run_tpch.py
+++ run_tpch.py
@@ -27,4 +27,20 @@ tpch = GrappaExperiment({
     'shared_pool_memory_fraction': 0.5
     })
 
+
+tpch = GrappaExperiment({
+    'trial': range(1, 3 + 1),
+    'qn': range(1, 22 + 1),
+    'exe': lambda qn: "grappa_tpc_q{0}.exe".format(qn),
+    'sf': 10,
+    'ppn': 16,
+    'nnode': 16,
+    'query': lambda qn: 'q{0}'.format(qn),
+    'vtag': 'v1',
+    'machine': 'pal'
+},
+{
+    'shared_pool_memory_fraction': 0.5
+    })
+
 tpch.run()
