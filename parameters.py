4270342b4b32c5633eeeef85af94befee23e6b0f
Brandon Myers <bdmyers@cs.washington.edu>
2016-04-27 17:56:54 -0700

    well I liked the mixins so keep them as an option

--- parameters.py
+++ parameters.py
@@ -20,5 +20,41 @@ class ParameterUtils(object):
         sys.stdout.flush()
 
 
+# An alternative to using ParameterUtils imperatively is to use them
+# declaratively. Mixin these classes to your Experiment subclass.
+# Each of these parameter mixins will be automatically chained together.
+
+
+def _call_super_if_exists(clazz, self, params):
+    if hasattr(super(clazz, self), 'recordparams'):
+        getattr(super(clazz, self))(params)
+
+
+class RequiredParams(object):
+    """Mixin class for subclasses of Experiment to provide
+    required parameters. To use, also provide class variable
+    _required
+    """
+    _required = []
+
+    def recordparams(self, params):
+        if len(self._required) == 0:
+            raise ValueError("Mixing in RequiredParams requires at least one required param")
+
+        ParameterUtils.require_params(self._required, params)
+
+        _call_super_if_exists(RequiredParams, self)
+
+
+class JSONParams(object):
+    """Mixin class for subclasses of Experiment to provide a
+    recordparams that outputs parameters as a JSON string
+    PARAMS{ "myparam": "paramvalue", ...}PARAM
+    """
+    def recordparams(self, params):
+        ParameterUtils.print_params_as_json(params)
+
+        _call_super_if_exists(RequiredParams, self)
+
 
 
